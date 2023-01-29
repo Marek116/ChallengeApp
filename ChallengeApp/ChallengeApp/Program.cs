19a28a0d60131347cddf5d6aba3ad76af409b04a
Marek116 <niebudkowskimarek@gmail.com>
2023-01-29 17:42:06 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string name1 = "EWA";
+string name2 = "PIOTR";
+char sex1 = 'K';
+char sex2 = 'M';
+var age1 = 25;
+var age2 = 16;
+
+
+
+
+// operatory relacyjne  ==, !=, <,>, <=, >=,
+
+//bool isActive = true;
+
+//if (age < 50)
+//{
+//    if (number1 == "Marek")
+      //  Console.WriteLine("MOJE IMIĘ MAREK i MAM POWYŻEJ 50");
+//}
+//else if (age > 40)
+//{
+   // Console.WriteLine("JESTEM osobą po 40");
+
+//}
+
+//operatory logiczne  && (i), || (lub), ! (negacja)
